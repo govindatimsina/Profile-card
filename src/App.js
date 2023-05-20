9c8f4e2502594e2c7db5c6a98cf234724ac83af5
Govin <timsinagovin17@gmail.com>
2023-05-20 12:54:47 +0545

    UI updated

--- src/App.js
+++ src/App.js
@@ -19,13 +19,16 @@ function App() {
     const actualData = await res.json();
     setData(actualData);;
   }
- 
- 
+
+  const handledecrementor = ()=>{
+    setIndex(index-1);
+  }
   return (
-    <div className="App">
+    <div className="mainbox">
       {/* {console.log(data)} */}
-      <button onClick={handleIncrementor}>Click</button>
-      <ProfileCard Id={data[index].id} Name={data[index].name} Email={data[index].email} Message={data[index].body}/>      
+      <button onClick={handledecrementor} className='prev'>{"<"}</button>
+      <ProfileCard Id={data[index].id} P_url={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOX2A9PishKybgn8eK6QARTKuP_Q8fgm7tX4Xnopr5&s"} Name={data[index].name} Email={data[index].email} Message={data[index].body}/>
+      <button onClick={handleIncrementor} className='next'> {">"} </button>
     </div>
   );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import './App.css'
+
+
 export default function ProfileCard({Id, P_url, Name, Email, Message}) {
   return (
     <div>
         <div className="maincontainer">
-            <img src={P_url} alt="IMG" />
+            <div className="imgcontainer">
+               <img src={P_url} alt="IMG" />
+            </div>
             <h3>Id: {Id?Id:""}</h3>
-            <h2>Name: {Name? Name: ""}</h2>
-            <h3>Email: {Email? Email: ""}</h3>
+            <h2>Name:</h2> <p>{Name? Name: ""}</p>
+            <h3>Email:</h3> <p><a href="mailto:{Email}">{Email? Email: ""}</a></p>
             <p><b>Message:</b> {Message? Message: ""}</p>
         </div>
     </div>
